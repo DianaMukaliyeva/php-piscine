38f1412c3ed9a3893f6b102292510c496f8c7254
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-03-12 20:19:23 +0200

    modified ex02

--- 02day/ex02/magnifying_glass.php
+++ 02day/ex02/magnifying_glass.php
@@ -5,10 +5,10 @@
 		$file_arr = file($argv[1]);
 		foreach($file_arr as $str)
 		{
-			if (preg_match('/(<a.*>.*<\/a>)/', $str, $matches) != 0)
+			if (preg_match('/(<a.*>.*<\/a>)/', $str, $match) != 0)
 			{
-				preg_match_all('([\'"].*?[\'"]|[\>].*?[\<])', $str, $matches);
-				foreach ($matches[0] as $needle)
+				preg_match_all('([\'"].*?[\'"]|[\>].*?[\<])', $match[0], $matches1);
+				foreach ($matches1[0] as $needle)
 					$str = str_replace($needle, strtoupper($needle), $str);
 			}
 			echo "$str";
