bb7669afa454d2a9cafbc1d2e7ea86b08a19a5e2
Diana <diana.mukaliyeva@gmail.com>
2020-03-17 14:53:48 +0200

    changes

--- 01day/ex10/do_op.php
+++ 01day/ex10/do_op.php
@@ -8,19 +8,12 @@
 	$x = trim($argv[1]);
 	$y = trim($argv[3]);
 	$sign = trim($argv[2]);
-	if (!is_numeric($x) || !is_numeric($y) || !strstr("+-/*%", $sign))
-	{
-		echo $x."|".$y."|".$sign."\n";
-		echo "Incorrect Parameters\n";
-	}
-	else if ($sign == '+')
+	if ($sign == '+')
 		echo $x + $y."\n";
 	else if ($sign == '-')
 		echo $x - $y."\n";
 	else if ($sign == '*')
 		echo $x * $y."\n";
-	else if (($sign == '/' || $sign == '%') && $y == 0)
-		echo "Incorrect Parameters\n";
 	else if ($sign == '/')
 		echo $x / $y."\n";
 	else if ($sign == '%')
--- 02day/ex01/one_more_time.php
+++ 02day/ex01/one_more_time.php
@@ -1,10 +1,22 @@
 #!/usr/bin/php
 <?php
+	function get_key($needle, $array)
+	{
+		foreach ($array as $elem){
+			if (in_array($needle, $elem)){
+				$key = array_search($elem, $array);
+				break;
+			}
+		}
+		return $key;
+	}
+
 	function error()
 	{
 		echo "Wrong Format\n";
 		exit();
 	}
+	
 	if ($argc == 1)
 		exit();
 	if($argc == 2)
@@ -12,16 +24,12 @@
 		$array = explode(" ", $argv[1]);
 		if (count($array) != 5)
 			error();
-		$day_small = array("1" => "lundi", "2" => "mardi", "3" => "mercredi", "4" => "jeudi", "5" => "vendredi", "6" => "samedi", "0" => "dimanche");
-		$day_big = array("1" => "Lundi", "2" => "Mardi", "3" => "Mercredi", "4" => "Jeudi", "5" => "Vendredi", "6" => "Samedi", "0" => "Dimanche");
-		$month_small = array("1" => "janvier","2" => "février","3" => "mars", "4" => "avril", "5" => "mai", "6" => "juin" , "7" => "juillet", "8" => "août", "9" => "septembre", "10" => "octobre", "11" => "novembre", "12" => "décembre");
-		$month_big = array("1" => "Janvier","2" => "Février","3" => "Mars", "4" => "Avril", "5" => "Mai", "6" => "Juin" , "7" => "Juillet", "8" => "Août", "9" => "Septembre", "10" => "Octobre", "11" => "Novembre", "12" => "Décembre");
-		$day = array_search($array[0], $day_small);
-		if (!$day)
-			$day = array_search($array[0], $day_big);
-		$month = array_search($array[2], $month_small);
-		if (!$month)
-			$month = array_search($array[2], $month_big);
+		$days = array("1" => ["lundi","Lundi"], "2" => ["mardi","Mardi"], "3" => ["mercredi","Mercredi"], "4" => ["jeudi","Jeudi"], "5" => ["vendredi","Vendredi"], "6" => ["samedi","Samedi"], "0" => ["dimanche","Dimanche"]);
+		$months = array("1" => ["janvier","Janvier"],"2" => ["février","Février","Fevrier","fevrier"],"3" => ["mars","Mars"], "4" => ["avril","Avril"],
+		"5" => ["mai","Mai"], "6" => ["juin","Juin"] , "7" => ["juillet","Juillet"], "8" => ["août","Août","Aout","aout"], "9" => ["septembre","Septembre"],
+		"10" => ["octobre","Octobre"], "11" => ["novembre","Novembre"], "12" => ["décembre","Décembre","Decembre","decembre"]);
+		$day = get_key($array[0], $days);
+		$month = get_key($array[2], $months);
 		$real_day = date('w', strtotime($array[3]."-".$month."-".$array[1]." ".$array[4]));
 		if ($real_day != $day || !checkdate($month, $array[1], $array[3]))
 			error();
