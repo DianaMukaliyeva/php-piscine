c9965caf5b15b995755684f08b1c7406086952bf
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-06-25 18:12:28 +0300

    Change d02

--- 02day/ex01/one_more_time.php
+++ 02day/ex01/one_more_time.php
@@ -1,9 +1,8 @@
 #!/usr/bin/php
 <?php
-	function get_key($needle, $array)
-	{
-		foreach ($array as $elem){
-			if (in_array($needle, $elem)){
+	function get_key($needle, $array) {
+		foreach ($array as $elem) {
+			if (in_array($needle, $elem)) {
 				$key = array_search($elem, $array);
 				break;
 			}
@@ -11,16 +10,14 @@
 		return $key;
 	}
 
-	function error()
-	{
+	function error() {
 		echo "Wrong Format\n";
 		exit();
 	}
-	
+
 	if ($argc == 1)
 		exit();
-	if($argc == 2)
-	{
+	if($argc == 2) {
 		$array = explode(" ", $argv[1]);
 		if (count($array) != 5)
 			error();
@@ -33,7 +30,6 @@
 		$month = get_key($array[2], $months);
 		$year = $array[3];
 		$time = explode(":", $array[4]);
-		// print_r($time);
 		if (count($time) != 3 || $date < 1 || $date > 31 || $year < 1970 ||
 			(!preg_match('/^([0-9]){2}$/', $time[0]) || $time[0] < 0 || $time[0] > 23) ||
 			(!preg_match('/^([0-9]){2}$/', $time[1]) || $time[1] < 0 || $time[1] > 59) ||
--- 02day/ex03/who.php
+++ 02day/ex03/who.php
@@ -2,11 +2,9 @@
 <?php
 	$fd = fopen("/var/run/utmpx", 'rb');
 	date_default_timezone_set("Europe/Helsinki");
-	while($file = fread(($fd), 628));
-	{;
+	while($file = fread(($fd), 628)) {
 		$type = unpack("i", $file, 296);
-		if ($type[1] == 7);
-		{;
+		if ($type[1] == 7) {
 			$name = unpack("a256user", $file, 0);
 			$name = preg_replace('/[[:^print:]]/', '', $name);
 			$data = unpack("a32tid", $file, 260);
@@ -14,6 +12,6 @@
 			$time = unpack("itime", $file, 300);
 			$time = date("M d H:i", $time['time']);
 			echo "$name[user] $data[tid]  $time \n";
-		};
-	};
+		}
+	}
 ?>
