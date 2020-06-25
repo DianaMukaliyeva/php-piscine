2c6196ef79505d00e9298936074bdfa34f6bba42
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-06-25 16:09:40 +0300

    Modify day 03

--- 03day/ex06/members_only.php
+++ 03day/ex06/members_only.php
@@ -1,5 +1,4 @@
 <?php
-
 	if (!isset($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_USER'] != 'zaz' || !isset($_SERVER['PHP_AUTH_PW']) || $_SERVER['PHP_AUTH_PW'] != 'Ilovemylittleponey') {
 		header('WWW-Authenticate: Basic realm=\'\'Member area\'\'');
 		header('HTTP/1.0 401 Unauthorized');
@@ -8,7 +7,7 @@
 	} else {
 		$file = base64_encode(file_get_contents("../img/42.png"));
 		echo ("<html><body>\n");
-		echo ("Hello {$_SERVER['PHP_AUTH_USER']}<br />\n");
+		echo ("Hello Zaz<br />\n");
 		echo ("<img src='data:image/png;base64,$file'>\n");
 		echo ("</body></html>");
 	}
