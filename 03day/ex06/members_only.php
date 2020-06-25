53dba9e03a679303a33e78117b36d47ee598e127
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-06-25 15:08:27 +0300

    Modify day03

--- 03day/ex06/members_only.php
+++ 03day/ex06/members_only.php
@@ -1,7 +1,7 @@
 <?php
 
 	if (!isset($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_USER'] != 'zaz' || !isset($_SERVER['PHP_AUTH_PW']) || $_SERVER['PHP_AUTH_PW'] != 'Ilovemylittleponey') {
-		header('WWW-Authenticate: Basic realm="Member area"');
+		header('WWW-Authenticate: Basic realm=\'\'Member area\'\'');
 		header('HTTP/1.0 401 Unauthorized');
 		echo "<html><body>That area is accessible for members only";
 		echo "</body></html>\n";
@@ -9,7 +9,7 @@
 		$file = base64_encode(file_get_contents("../img/42.png"));
 		echo ("<html><body>\n");
 		echo ("Hello {$_SERVER['PHP_AUTH_USER']}<br />\n");
-		echo ("<img src=data:image/png;base64,$file'>\n");
+		echo ("<img src='data:image/png;base64,$file'>\n");
 		echo ("</body></html>");
 	}
 ?>
