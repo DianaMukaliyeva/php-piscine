5b3f736fa2753910045783218dde1e6e8e6bb8cd
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-06-26 22:16:45 +0300

    Complete day 03

--- 03day/ex03/cookie_crisp.php
+++ 03day/ex03/cookie_crisp.php
@@ -5,15 +5,14 @@
 			setcookie($vars["name"], $vars["value"], time() + (60 * 60 * 24 * 30), '/');
 			break;
 		case "get":
-			if (!empty($vars["name"]))
-			{
+			if ($vars["name"]) {
 				$name = $vars["name"];
-				if(isset($_COOKIE[$name]))
+				if($_COOKIE[$name])
 					echo "$_COOKIE[$name]\n";
 			}
 			break;
 		case "del":
-			if (!empty($vars["name"]) && !isset($vars["value"])) {
+			if ($vars["name"] && $vars["value"]) {
 				setcookie($vars["name"], "", time() - 3600);
 			}
 			break;
--- 03day/ex06/members_only.php
+++ 03day/ex06/members_only.php
@@ -1,14 +1,15 @@
 <?php
-	if (!isset($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_USER'] != 'zaz' || !isset($_SERVER['PHP_AUTH_PW']) || $_SERVER['PHP_AUTH_PW'] != 'Ilovemylittleponey') {
+	if (!($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_USER'] != 'zaz' || !($_SERVER['PHP_AUTH_PW']) || $_SERVER['PHP_AUTH_PW'] != 'Ilovemylittleponey') {
 		header('WWW-Authenticate: Basic realm=\'\'Member area\'\'');
 		header('HTTP/1.0 401 Unauthorized');
-		echo "<html><body>That area is accessible for members only";
-		echo "</body></html>\n";
+		echo "<html><body>";
+		echo "That area is accessible for members only";
+		echo "</body></html>     \n";
 	} else {
 		$file = base64_encode(file_get_contents("../img/42.png"));
 		echo ("<html><body>\n");
 		echo ("Hello Zaz<br />\n");
 		echo ("<img src='data:image/png;base64,$file'>\n");
-		echo ("</body></html>");
+		echo ("</body></html>\n");
 	}
 ?>
