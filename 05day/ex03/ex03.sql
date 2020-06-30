17561c6f820616bcd57d92e12b39ac1343f691f8
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-06-30 17:40:43 +0300

    modify ex03 d05

--- 05day/ex03/ex03.sql
+++ 05day/ex03/ex03.sql
@@ -1,6 +1,6 @@
 INSERT INTO db_dmukaliy.ft_table(`login`, `group`, `creation_date`)
 SELECT `last_name` AS `login`, 'other' AS `group`, `birthdate` AS `creation_date`
-FROM db_dmukaliy.user_card
+FROM `user_card`
 WHERE
     (length(`last_name`) < 9) AND
     (`last_name` LIKE '%a%')
