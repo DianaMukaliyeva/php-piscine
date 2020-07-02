92ee00029b0ca54741059f9fbcba3693391bc220
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-07-02 19:48:41 +0300

    day07 ex02

---
+++ 07day/ex02/Targaryen.class.php
@@ -0,0 +1,14 @@
+<?php
+    class Targaryen{
+        public function resistsFire(){
+            return False;
+        }
+       
+        public function getBurned()
+        {
+            if ($this->resistsFire())
+                return "emerges naked but unharmed";
+            return "burns alive";
+        }
+    }
+?>
