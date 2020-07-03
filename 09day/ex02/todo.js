741871db744e6e5a276f76d919ba1b96f0c88578
Diana Mukaliyeva <diana.mukaliyeva@gmail.com>
2020-07-03 17:51:04 +0300

    Add day 09 ex02

---
+++ 09day/ex02/todo.js
@@ -0,0 +1,37 @@
+let cookies = document.cookie.split(';');
+let i = document.cookie ? cookies.length : 0;
+
+if (document.cookie) {
+    cookies.forEach((element, index) => {
+        i = element.split('=')[0].trim();
+        addToDom(element.split('=')[1], i);
+    });
+}
+
+document.getElementById('new').addEventListener('click', e => {
+    e.preventDefault();
+    const task = prompt("Your new task is:")
+    if (task && task.trim()) {
+        i++;
+        addToDom(task, i);
+        document.cookie = i + "=" + task;
+    }
+})
+
+function addToDom(task, id) {
+    const parent = document.getElementById('ft_list');
+    let newTask = document.createElement('div');
+    newTask.innerHTML = task;
+    newTask.setAttribute('id', id);
+    newTask.setAttribute('onclick', "delTask(" + id + ")");
+    parent.insertBefore(newTask, parent.firstChild);
+}
+
+function delTask(id) {
+    if (confirm("Are you sure you want to delete this task?")) {
+        let parent = document.getElementById("ft_list");
+        let task = document.getElementById(id);
+        parent.removeChild(task);
+        document.cookie = id + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    }
+}
